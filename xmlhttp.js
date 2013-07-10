11023dd176a5d68214e374567276f0885ac99e21
板橋 清信 <keyi8773@yahoo.co.jp>
2013-07-10 14:51:23 +0900

    Initial commit

---
+++ xmlhttp.js
@@ -0,0 +1,26 @@
+// HTTPí êMópÅAã§í ä÷êî
+function createXMLHttpRequest(cbFunc)
+{
+	var XMLhttpObject = null;
+	try{
+		XMLhttpObject = new XMLHttpRequest();
+	}catch(e){
+		try{
+			XMLhttpObject = new ActiveXObject("Msxml2.XMLHTTP");
+		}catch(e){
+			try{
+				XMLhttpObject = new ActiveXObject("Microsoft.XMLHTTP");
+			}catch(e){
+				return null;
+			}
+		}
+	}
+	if (XMLhttpObject) XMLhttpObject.onreadystatechange = cbFunc;
+	return XMLhttpObject;
+}
+
+// document.getElementById
+function $(tagId)
+{
+	return document.getElementById(tagId);
+}
